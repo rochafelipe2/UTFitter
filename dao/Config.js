656397f8049c0edd7090d8639bee1493b1a4d12a
Felipe Rocha <lipdiso@gmail.com>
2019-11-13 00:48:25 -0300

    Agora vai

--- dao/Config.js
+++ dao/Config.js
@@ -1,5 +1,5 @@
 module.exports ={
-    uri: 'mongodb://127.0.0.1:27017/',
+    uri: 'mongodb+srv://appmongo:openopen@cluster0-udzfn.mongodb.net/test?retryWrites=true&w=majority/',
     db: 'utfitterDB',    
     options:{
       //  userNewUrlParser: true
